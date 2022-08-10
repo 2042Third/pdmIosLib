27552744c7da1c2f7d166d38495d9db8159edf35
Mike Yang <18604713262@163.com>
2022-08-09 20:24:47 -0400

    Initial Commit

---
+++ pdm-crypt/pdm_crypt.h
@@ -0,0 +1,12 @@
+//
+//  pdm_crypt.h
+//  pdm-crypt
+//
+//  Created by MikeYang on 8/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface pdm_crypt : NSObject
+
+@end
---
+++ pdm-crypt/pdm_crypt.m
@@ -0,0 +1,12 @@
+//
+//  pdm_crypt.m
+//  pdm-crypt
+//
+//  Created by MikeYang on 8/9/22.
+//
+
+#import "pdm_crypt.h"
+
+@implementation pdm_crypt
+
+@end
